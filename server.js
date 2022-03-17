0c95d66ed0406ef20b1505676ca58b9dbd6075ad
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-03-17 12:05:14 +0530

    Calling the connectdb to connect server

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // getting express framework
 const express = require ('express');
+const connectDB = require ('./config/db');
+
 //declaring express to a variable 'app' to access it.
 const app = express ();
+
+//connecting the database. Calling the connectDB
+connectDB();
 
 app.get ('/', (req, res) => {
     res.send("ticket booking app");
@@ -9,6 +14,7 @@ app.get ('/', (req, res) => {
 });
 
 const PORT= process.env.PORT || 5001;
+
 //app has to listen the port on the PORT variable(5001)
 app.listen (PORT, () =>{
     console.log(`Server started at port ${PORT}`)
