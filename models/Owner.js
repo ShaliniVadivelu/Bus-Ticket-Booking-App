8069e17d3b406055526bfb7d69373c14640dc3ba
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-03-21 22:50:08 +0530

    added role to owner

--- models/Owner.js
+++ models/Owner.js
@@ -20,6 +20,12 @@ const ownerSchema = new mongoose.Schema(
     },
     email: {
       type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      required: true,
+      ref : "../../config/data"
     },
     password: {
       type: String,
