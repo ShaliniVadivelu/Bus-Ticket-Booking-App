086ca2cdffc2596c0f4b9541c28dc333af790214
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-03-27 22:19:51 +0530

    Added objects

--- routes/api/bus.js
+++ routes/api/bus.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
 const auth = require('../../middleware/auth');
-const {check, validationResult} = require('express-validator');
 const Owner = require('../../models/Owner');
 const Bus = require('../../models/Bus');
+const {ROLE} = require('../../config/data');
 
 // @route     POST api/bus
 // @desc      Creating new bus and its details
@@ -12,14 +13,14 @@ const Bus = require('../../models/Bus');
 
 router.post('/',[auth,
     [
-    check('owner', 'Owner is required').not().isEmpty(),
+    check('companyName', 'Company name is required').not().isEmpty(),
     check('busType', 'Please include the bus type').not().isEmpty(),
     check('busNumber', 'Please include the bus number').not().isEmpty().isNumeric(),
     check('startCity', 'Please include the start city').not().isEmpty(),
     check('destination', 'Please include the destination city').not().isEmpty(),
     check('totalSeats', 'please include the total no.of seats').not().isEmpty().isNumeric()
-]
-],
+    ]
+], 
  async (req, res) => {
 
     const errors = validationResult(req);
@@ -30,11 +31,12 @@ router.post('/',[auth,
     }
 
     const{
-        owner,
+        companyName,
         busType,
         busNumber,
         startCity,
         destination,
+        totalSeats,
         availableSeats,
         pricePerSeat,
         departureDate,
@@ -42,15 +44,45 @@ router.post('/',[auth,
         duration
     }=req.body;
 
-    try {
+    // Build profile object
+    const busFields = {};
+    
+    busFields.user =  req.user.id;
+    if(companyName) busFields.companyName =  companyName;
+    if(busType) busFields.busType =  busType;
+    if(busNumber) busFields.busNumber =  busNumber;
+    if(startCity) busFields.startCity =  startCity;
+    if(destination) busFields.destination =  destination;
+    if(totalSeats) busFields.totalSeats =totalSeats;
+    
         let bus = await Bus.findOne({busNumber});
 
         if(bus)
         {
             return res.status(400).json({errors:[{msg: 'Bus already exist'}]});
         }
-        res.send('Bus route');
         
+        try {
+            
+        let bus = await Bus.findOne({user: req.user.id});
+        
+        if(bus) {
+            // Update   
+            bus = await Bus.findOneAndUpdate(
+                { user: req.user.id}, 
+                {$set: busFields},
+                {new: true}
+            );
+
+            return res.json (bus);
+        }
+
+        //Create    
+        bus = new Bus(busFields);
+
+        await bus.save();
+        res.json(bus);
+
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
