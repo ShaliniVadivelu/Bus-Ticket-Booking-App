a5eeb8b2f0974179e8e23d7318cb1dcc36364c47
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-03-28 23:18:32 +0530

    separate midlleware for owner

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const ROLE = require ('../config/data');
 
-module.exports = function(req, res, next) {
+module.exports = function authBasic (req, res, next) {
 
     const token = req.header('x-auth-token');
 
@@ -11,11 +12,40 @@ module.exports = function(req, res, next) {
 
     try {
         const decoded = jwt.verify(token, config.get('jwtSecret'));
-
+ 
         req.user = decoded.user;
 
         next();
     } catch (err) {
         res.status(401).json( { msg: 'Token is not valid'});
     }
-}
+};
+
+module.exports = function authOwner (req, res, next) {
+
+    const token = req.header('x-auth-token');
+
+    if(!token) {
+        return res.status(401).json({ msg: 'No token, authoroization denied' });
+    }
+    
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+ 
+        req.owner = decoded.owner;
+        next();
+    } catch (err) {
+        res.status(401).json( { msg: 'Token is not valid'});
+    }
+};
+
+// module.exports = function authRole (role) {
+//     return (req, res, next) => {
+//       if (ROLE.ADMIN !== role) {
+//         res.status(401)
+//         return res.send('Not allowed')
+//       }
+//       next()
+//     }
+//   };
+
