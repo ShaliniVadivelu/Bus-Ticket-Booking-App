e56ada1746265ee183ae51da5a04f974ccd5a7ab
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-03-31 13:17:06 +0530

    Search bus with 3 fields

--- routes/api/booking.js
+++ routes/api/booking.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 const {check, validationResult} = require ('express-validator');
 
-router.post ('/searchBus', [
-    check('startCity', 'startCity is required').not().isEmpty(),
-    check('destination', 'destination is required').not().isEmpty(),
+const Bus = require('../../models/Bus');
+
+router.get ('/searchBus', [
+    check('startCity', 'StartCity is required').not().isEmpty(),
+    check('destination', 'Destination is required').not().isEmpty(),
+    check('departureDate', "Departure date is required").not().isEmpty()
+    
 ], async (req, res)=>{
 
     const errors = validationResult(req);
@@ -15,8 +18,31 @@ router.post ('/searchBus', [
         return res.status(400).json({errors: errors.array()});
     }
 
-    res.send('dafd');
-    
+    try {
+        // $and is to combine both fields for filtering
+    const bus = await Bus.find({
+        $and: [
+            {
+                $and: [
+                    {startCity: req.body.startCity}, 
+                    {destination: req.body.destination}
+                ]
+            },
+                {departureDate: req.body.departureDate}
+            ]
+    });
+
+    if(!bus)
+    {
+        console.log('Bus not found');
+        return res.status(400).json({msg :'Bus not found'});
+    }
+    res.json(bus);
+
+    } catch(err) {
+        console.error (err.message);
+        res.status(500).send ('Server error');
+    }
 });
 
 module.exports = router;
