2480875379079195e228496df195d440c67c0222
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-04 18:56:51 +0530

    User part(register, login)

--- routes/api/user.js
+++ routes/api/user.js
@@ -7,13 +7,12 @@ const config = require('config');
  
 const {check, validationResult} = require('express-validator');
 const User = require('../../models/User');
-const Owner = require('../../models/Owner');
 const {ROLE} = require ('../../config/data');
 
 // @route     POST api/users
 // @desc      Register user
 // @access    Public
-router.post ('/basic', [
+router.post ('/', [
     check ('name', 'Please give your name without any numeric').not().isEmpty().isAlpha(),
     check ('email', 'Please include a valid email').isEmail(),
     check ('role', 'Role is required').not().isEmpty(),
@@ -84,69 +83,86 @@ router.post ('/basic', [
     }
 });
 
-// @route     POST api/user
-// @desc      Register Admin
+// @route     POST api/user/login
+// @desc      Authenticate  user & get token
 // @access    Public
-router.post ('/admin', [
-    check ('companyName', 'Please include your company name').not().isEmpty(),
-    check ('officeAddress', 'Please include your office address').not().isEmpty(),
-    check ('role', 'Role is required').not().isEmpty(),
-    check ('email','Please include your valid email').isEmail(),
-    check ('phone', 'Please include a valid contact number').isNumeric(),    
-    check('password','Please enter a password with 6 or more characters with alphanumeric').isLength({min:6}).isAlphanumeric()
-], async (req,res) => {
+router.post(
+    '/login',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password','Password is required').exists(),
+        check('role','Role is required').not().isEmpty(),
+],
+async (req, res) => {
     const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
-    }   
-
-    const {companyName,officeAddress,role,phone,email,password}=req.body;
+    
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array() });
+    }
     
     try {
-        
-        const avatar = gravatar.url(companyName,{
-            s:'200',
-            r:'pg',
-            d:'mm'
-        });
-        
-        let owner = new Owner ({
-            companyName,
-            officeAddress,
-            phone,
-            email,  
-            role,
-            password
-        });
-        
-        const salt = await bcrypt.genSalt(10);    
+    const {email,password,role} = req.body;
+    
+    // see if owner exists
+    let user= await User.findOne({email});
 
-        owner.password = await bcrypt.hash(password, salt);  
+    if (!user) 
+    {
+        return res.status (400).json({errors: [ { msg: 'Invalid Credentials'}]});
+    }
+ 
+    const isMatch = await bcrypt.compare (password, user.password);
 
-        await owner.save();
+    if(!isMatch) 
+    {
+        return res
+            .status (400)
+            .json({errors: [ { msg: 'Invalid Credentials'}]});
+    }
 
-        const payload ={
-            owner: {
-                id: owner.id,
-                role: ROLE.ADMIN
-            }
-        };
-        
-        jwt.sign(
-            payload,
-            config.get('jwtSecret'),
-            {expiresIn: 360000},
-            (err,token)=>{
-                if(err)
-                    throw err;
-                res.json({token});
-                }    
-            );
+    const payload = {
+        user: {
+            id: user.id,
+            role: ROLE.BASIC
+        }
+    };
 
+    jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token)=> {
+            if(err) throw err;
+            res.json({ token });
+        }
+        );
+
+    } catch (err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+
+// @route     GET api/user/me
+// @desc      Get current users profile
+// @access    Private
+
+router.get('/me/:_id', async(req, res) => {
+    try {   
+        const user = await User.findOne({user: req.params._id})
+                               .populate('bookings' )
+                               .select('-password');
+
+        if (!user)
+        {
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        }
+        res.json(user);
     } catch (err) {
         console.error(err.message);
-        return res.status(500).send('Server error');
+        res.status(500).send('Server error');
+        
     }
 });
 
