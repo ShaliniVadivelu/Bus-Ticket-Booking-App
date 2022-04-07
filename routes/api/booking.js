26a9c49b34c932f1ddf01add615d29c8fc30709a
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-07 10:01:56 +0530

    Ticket booking route

--- routes/api/booking.js
+++ routes/api/booking.js
@@ -3,89 +3,66 @@ const router = express.Router();
 const {check, validationResult} = require ('express-validator');
 
 const Bus = require('../../models/Bus');
+const Booking = require('../../models/Booking');
 const auth = require('../../middleware/auth');
 
-// @route     GET api/booking/searchBus
-// @desc      search the bus with 3 fields
-// @access    Public
+// @route     POST api/bus/BookTicket
+// @desc      Booking a ticket
+// @access    Private
 
-router.get ('/searchBus', [auth.authBasic,
+router.post ('/:bus_id/bookTicket',[auth.authBasic,
     [
-    check('startCity', 'StartCity is required').not().isEmpty(),
-    check('destination', 'Destination is required').not().isEmpty(),
-    check('departureDate', "Departure date is required").not().isEmpty()
+    check('numPassengers', 'Please enter no.of passengers').not().isEmpty().isNumeric(),
     ]
-], async (req, res)=>{
+],async (req,res) => {
 
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty())
+    const errors= validationResult(req);
+    if (!errors.isEmpty())
     {
-        return res.status(400).json({errors: errors.array()});
+        return res.status(400).json({errors: errors });
     }
 
+    const {numPassengers} = req.body;
+    
+    let bookBus = await Bus.findOne({bus: req.params.bus_id}).select('busNumber startCity destination pricePerSeat');
+    
+    if(numPassengers > bookBus.availableSeats)
+    {
+        return res.status(401).json({msg: 'given no.of seats are greater than available seats'});
+    }
+    
+    const ticketFields = {};
+
+    ticketFields.bus = bookBus;
+    const msg = {};
+
+    if (bookBus.busNumber) msg.busNumber=bookBus.busNumber; 
+    if (bookBus.startCity) msg.startCity = bookBus.startCity;
+    if (bookBus.destination) msg.destination = bookBus.destination;
+    if (bookBus.startCity) msg.startCity = bookBus.startCity; 
+    if (numPassengers) 
+    {
+        ticketFields.numPassengers = numPassengers;
+    }
+    if (bookBus.pricePerSeat) 
+    {
+        ticketFields.pricePerSeat = bookBus.pricePerSeat;  
+    }
+    ticketFields.totalPrice = numPassengers * bookBus.pricePerSeat;
+
+    ticketFields.bookingStatus = 'BOOKED';
+    
     try {
-        // $and is to combine both fields for filtering
-    const bus = await Bus.find({
-        $and: [
-            {
-                $and: [
-                    {startCity: req.body.startCity}, 
-                    {destination: req.body.destination}
-                ]
-            },
-                {departureDate: req.body.departureDate}
-            ]
-    });
 
-    if(!bus) {
-        return res.status(400).json({error: [{msg :'No bus found!'}]});
-       }
-    res.json(bus);
-    
-    } catch(err) {
-        console.error (err.message);
-        res.status(500).send ('Server error');
+        ticket = new Booking(ticketFields);
+        await ticket.save();
+        res.json(ticket);
+
+    } catch(err){
+        console.error(err.message);
+        return res.status(500).send('Server error');
     }
-});
-
-// // @route     POST api/bus/BookTicket
-// // @desc      Booking a ticket
-// // @access    Private
-
-// router.post ('/bus/:bus_id/bookTicket',[auth.authBasic,
-//     check('numPassengers', 'Please enter no.of passengers').not().isEmpty().isNumeric(),
-//     check("seatNumber", "Please select the seat").not().isEmpty()
-// ],async (req,res) => {
-
-//     const errors= validationResult(req);
-//     if (!errors.isEmpty())
-//     {
-//         return res.status(400).json({errors: errors.array });
-//     }
     
-//     const { numPassengers, seatNumber} = req.body;
-
-//     const ticketFields = {};
-//     ticketFields.user = req.user.id;
-//     ticketFields.bus =  req.bus.id;
-//     if(numPassengers) ticketFields.numPassengers =numPassengers;
-//     if(seatNumber) {
-//         ticketFields.seatNumber =  seatNumber.split(',').map(seatNumber => seatNumber.trim());
-//     }
-//     try {
-//         let bookTicket = await Bus.findOne({ bus: req.bus.id });
-
-//         bookTicket = new Profile(ticketFields);
-
-//         await bookTicket.save();
-//         res.json(bookTicket);
-
-//     }catch (err)
-//     {
-//         console.error(err.message);
-//         res.status(500).send('Server Error');
-//     }
-// });
+});
 
 module.exports = router;
