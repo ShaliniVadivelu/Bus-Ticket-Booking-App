5490fedfff37e7f5e106633c700f23a3cf9646bf
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-12 17:45:38 +0530

    Landing page of the application

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Landing =() => {
+    return (
+        <section className="landing">
+          <div className="landing-inner">
+           <h1 className="x-large">Saftey! Healthy! Happy! travel with Happy Bus</h1>
+            <p className="lead">
+              Our website will help you to book a bus ticket for personal and official use.
+            </p>
+          <div className="buttons">
+            <a className="btn btn-primary" href="register.html"> Sign Up </a>
+            <a className="btn btn-light" href="login.html"> Login </a>
+          </div>
+        </div>  
+    </section>
+    )
+}
+
+export default Landing
