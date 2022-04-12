fa05a3c721872068ab5c071d23d2b0374ff4038b
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-12 18:22:04 +0530

    reinstalled older version (5.2.0) of react-router-dom

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const login = () => {
+    return 
+    <div> Login Page </div>
+};
+
+export default login
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Register = () => {
+    return 
+    <div> Register Page </div>
+};
+
+export default Register
