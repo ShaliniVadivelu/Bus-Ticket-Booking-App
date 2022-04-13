ebc33d28eb21a71607b3c7cf097af86bd490eb10
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-13 18:48:44 +0530

    added separte route for owner and user register and login

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,27 @@ import React, { Fragment } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import UserRegister from './components/auth/UserRegister';
+import UserLogin from './components/auth/UserLogin';
+import OwnerRegister from './components/auth/OwnerRegister';
+import OwnerLogin from './components/auth/OwnerLogin';
+
 import './App.css';
 
-const App = () => {
-  <Router>
+const App = () => (
+    <Router>
     <Fragment>
-      <Navbar />
-      <Route exact path ='/' component ={Landing} />
-      <section>
+      <Navbar />   
+      <Route exact path='/' component={Landing} />
+      <section className ='container'>
         <Switch>
-          <Route exact path ='/' component ={Register} />
-          <Route exact path ='/' component ={Login} />
-         </Switch>
+        <Route exact path='/userregister' component={UserRegister} />
+        <Route exact path='/userlogin' component={UserLogin} />
+        <Route exact path='/ownerregister' component={OwnerRegister} />
+        <Route exact path='/ownerlogin' component={OwnerLogin} />
+        </Switch>
       </section>
     </Fragment>
-  </Router>
-}
+    </Router>
+);
 export default App;
