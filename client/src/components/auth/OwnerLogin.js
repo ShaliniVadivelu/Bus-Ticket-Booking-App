a9678c8fea299c4ca85756f8a0098bd4e8e33892
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-13 18:49:27 +0530

    test owner login and register

---
+++ client/src/components/auth/OwnerLogin.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const OwnerRegister = () => {
+    return (
+        <section className='BgPic'>
+            <Fragment>
+                <h1 className="large text-primary">Sign In</h1>
+            </Fragment>
+        </section>
+    )
+};
+
+export default OwnerRegister;
