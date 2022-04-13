aeb665440465ae8aefe05f6813f695d52ede2967
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-13 18:49:55 +0530

    test user login and register

---
+++ client/src/components/auth/UserRegister.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+const UserRegister = () => {
+    return (
+    <section className='BgPic'>
+        <Fragment> 
+            <h1 className="large text-primary">Sign Up</h1>
+        </Fragment>
+    </section>
+    )
+};
+
+export default UserRegister;
