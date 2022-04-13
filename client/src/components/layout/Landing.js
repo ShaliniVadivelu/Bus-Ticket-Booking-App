d779e4504fa9899ea2ae716fa099ca6088c7a085
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-13 18:47:33 +0530

    Landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Landing =() => {
     return (
         <section className="landing">
           <div className="landing-inner">
-           <h1 className="x-large">Saftey! Healthy! Happy! travel with Happy Bus</h1>
+           <h1 className="x-large">Safety! Healthy! Happy! travel with <h1 className="large">Happy Bus</h1></h1>
             <p className="lead">
               Our website will help you to book a bus ticket for personal and official use.
             </p>
           <div className="buttons">
-            <a className="btn btn-primary" href="register.html"> Sign Up </a>
-            <a className="btn btn-light" href="login.html"> Login </a>
+            <Link className="btn btn-primary" to="/register"> Sign Up </Link>
+            <Link className="btn btn-light" to="/login"> Login </Link>
           </div>
         </div>  
     </section>
     )
 }
 
-export default Landing
+export default Landing;
