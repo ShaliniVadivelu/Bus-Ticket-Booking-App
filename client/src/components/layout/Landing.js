6f223b6c7dc225e919fea091aaac733b77faab45
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-18 20:06:32 +0530

    modified classname

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,13 +6,13 @@ const Landing =() => {
         <section className="landing">
           <div className="landing-inner">
            <h1 className="x-large">Safety! Healthy! Happy! travel with <h1 className="large">Happy Bus</h1></h1>
-            <p className="lead">
+            <p className="mainlead">
               Our website will help you to book a bus ticket for personal and official use.
             </p>
-          <div className="buttons">
+          {/* <div className="buttons">
             <Link className="btn btn-primary" to="/register"> Sign Up </Link>
             <Link className="btn btn-light" to="/login"> Login </Link>
-          </div>
+          </div> */}
         </div>  
     </section>
     )
