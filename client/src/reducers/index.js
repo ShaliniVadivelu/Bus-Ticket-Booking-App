f34b026f27ec8b1463245036efe1fcbfdb96632c
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-18 21:27:03 +0530

    auth alert

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 //rootReducers
 import { combineReducers } from "redux";
 import alert from './alert';
+import auth from './auth';
 
 //The combineReducers helper function turns an object whose values are different reducing functions into a single reducing function you can pass to createStore. reducers called as reducing fn used to reduce the collection of values down to single value
 export default combineReducers ({
-    alert 
+    alert,
+    auth
 });
