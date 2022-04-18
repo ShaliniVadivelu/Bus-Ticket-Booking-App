471bb6246f78a2156b125fbd875ed92fa23d92ed
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-18 20:05:44 +0530

    Added comments

--- client/src/components/auth/UserRegister.js
+++ client/src/components/auth/UserRegister.js
@@ -1,5 +1,7 @@
 import React, {Fragment, useState} from 'react';
 import {Link} from 'react-router-dom';
+
+//The connect() function connects a React component to a Redux store.
 import {connect} from 'react-redux';
 import { setAlert } from '../../actions/alert';
 import PropTypes from 'prop-types';
@@ -7,8 +9,10 @@ import PropTypes from 'prop-types';
 // useState is a hook which allows us to have a state variable in functional components and it returns a pair: one is a current state value and a function that lets us update it.
 
 const UserRegister = ({setAlert}) => {
-// formData is a state(which has field values) and setFormData is a fn used to udpate the state values
+
+// formData is a current state(which has field values) and setFormData is a fn used to udpate the state values
 const [formData, setFormData] = useState({
+    //passing the default objects as name....all empty in the inital(initial state)
     name : '',
     email : '',
     role: '',
@@ -18,14 +22,18 @@ const [formData, setFormData] = useState({
     password2 : '' 
 });
 
+// pulling all the info from formData
     const {name,email,role,phone,gender,dob,text,password,password2}=formData;
 
+//using spread operator to copy the formData
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
      const onSubmit = async e => {
+         //we cannot return false to prevent default behaviour in react. we have to call it explicitly
+         //e-> synthetic event
          e.preventDefault();
          if(password !== password2){
-            setAlert('Passwords do not match')
+            setAlert('Passwords do not match','danger');
          }
          else
          {
