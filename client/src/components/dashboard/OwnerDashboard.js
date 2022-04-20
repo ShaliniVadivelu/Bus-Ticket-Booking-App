3a390a3447be3c393c0bbd945cd4f9cd590e27a6
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-20 18:29:23 +0530

    test owner dashboard

---
+++ client/src/components/dashboard/OwnerDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const OwnerDashboard = props => {
+    return  <div className='container'>
+            Shalini Owner Dashboard;
+        </div>
+};
+
+OwnerDashboard.propTypes ={
+
+}
+
+export default OwnerDashboard;
