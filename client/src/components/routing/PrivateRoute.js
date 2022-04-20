320cc82533943dad6897c8de0822bd1360f1c78c
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-20 14:12:04 +0530

    Private route

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,31 @@
+//when we loged out the account and go back to the page we can see the previous datas, but this should not happen. To avoid this we have create a private route
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+// rest operator, it will take anything was passed in
+const PrivateRoute = ( { component : Component, 
+    auth: { isAuthenticated, loading},
+    ...rest 
+}) => (
+    <Route 
+        { ...rest} 
+        render = { props=> 
+            !isAuthenticated && !loading ? (
+                <Redirect to='/userLogin' />
+                ) : (
+                    <Component {...props} />
+                    )
+        } 
+    />
+);
+
+PrivateRoute.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect (mapStateToProps) (PrivateRoute)
