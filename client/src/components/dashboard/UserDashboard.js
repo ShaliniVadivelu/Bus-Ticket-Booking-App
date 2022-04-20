227d69205df05f8516a5f058d3f0211f3b1d8eac
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-20 13:23:54 +0530

    test dashboard

---
+++ client/src/components/dashboard/UserDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserDashboard = props => {
+    return  <div className='container'>
+            Shalini User Dashboard;
+        </div>
+};
+
+UserDashboard.propTypes ={
+
+}
+
+export default UserDashboard;
