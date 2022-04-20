878a7d1112ffa354fdd42eb83ce99a2a4b92034b
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-04-20 18:27:46 +0530

    axios - owner register,login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,6 +4,7 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
+    OWNER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL
@@ -95,3 +96,90 @@ try {
     });
 }
 }
+
+// Load owner
+export const loadOwner = () => async dispatch => {
+    if (localStorage.token) {
+        setAuthToken (localStorage.token);
+    } 
+ 
+ try {
+     const res = await axios.get('/api/auth/owner');
+     dispatch ({
+         type: OWNER_LOADED,
+         payload: res.data
+     });
+} catch (err) {
+    dispatch({
+        type: AUTH_ERROR
+    });
+ }
+};
+
+//Register Owner
+export const ownerRegister = ({name,email,role,officeAddress,phone,password}) => async dispatch =>{
+    const config = {
+        headers:  {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify ({name,email,role,officeAddress,phone,password});
+
+try {
+
+    const res = await axios.post('/api/owner', body, config);
+
+    dispatch ({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+    });
+
+    dispatch(loadOwner());
+    
+}catch (err) {
+    const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch (setAlert(error.msg, 'danger')));
+        }
+
+    dispatch ({
+        type: REGISTER_FAIL
+    });
+}
+};
+
+//Login User
+export const ownerLogin = (email,role,password) => async dispatch =>{
+    const config = {
+        headers:  {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify ({email,role,password});
+
+try {
+
+    const res = await axios.post('/api/owner/login', body, config);
+
+    dispatch ({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+    });
+
+    dispatch(loadUser());
+}catch (err) {
+    const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch (setAlert(error.msg, 'danger')));
+        }
+
+    dispatch ({
+        type: LOGIN_FAIL
+    });
+}
+}
+
