5655b6058a991a93b8890fc77abe6d83be494953
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-05-04 13:23:00 +0530

    added profile to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,13 @@
 import { combineReducers } from "redux";
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
+import bus from './bus';
 
 //The combineReducers helper function turns an object whose values are different reducing functions into a single reducing function you can pass to createStore. reducers called as reducing fn used to reduce the collection of values down to single value
 export default combineReducers ({
     alert,
-    auth
+    auth,
+    profile,
+    bus
 });
