1f2bbcb3ab0e25d58113804f4f5847df47dc9985
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-05-04 13:21:34 +0530

    profile actions

---
+++ client/src/actions/profile.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { setAlert } from "./alert";
+
+import {
+    GET_PROFILE,
+    PROFILE_ERROR
+} from './types';
+
+// Get current owner profile
+export const getOwnerProfile = () => async dispatch => {
+
+    try {
+        const res = await axios.get('/api/owner/me/:id'); 
+
+        dispatch ({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+    } catch (err) {
+
+        dispatch ({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statuText, status: err.response.status}
+        })
+        
+    }
+
+}
