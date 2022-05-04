b460a9c33c7ec2e3d8f6b2c0b7ab2fc7b675af2c
Shaini Srirama V <shalinisrirama.v@gmail.com>
2022-05-04 13:23:29 +0530

    reducer bus

---
+++ client/src/reducers/bus.js
@@ -0,0 +1,15 @@
+const initialState = {
+    bus: null,
+    buses: [],
+    loading: true,
+    error: {}
+}
+
+export default function(state= initialState, action) {
+    const {type, payload} = action;
+
+    switch(type) {
+        
+    }
+
+}
